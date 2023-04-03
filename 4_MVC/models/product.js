85ebf57768669e0bd381a7af50fc31e04c5250db
ghazian_f_fakhrusy <ghazian_f_fakhrusy@telkomsel.co.id>
2023-04-03 11:40:48 +0700

    Edit Product Data

---
+++ 4_MVC/models/product.js
@@ -0,0 +1,66 @@
+const fs = require('fs');
+const path = require('path');
+
+// the path for storing data into "products.json" in "data" folders
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+// helper function to read File
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+// CLASS Product
+module.exports = class Product {
+  constructor(id, title, imageUrl, price, description) {
+    this.id = id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.price = price;
+    this.description = description;
+  }
+
+  // storing data in files via models
+  save() {
+    getProductsFromFile((products) => {
+      if (this.id) {
+        const existingProductIndex = products.findIndex(
+          (prod) => prod.id === this.id
+        );
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+          console.log(err);
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          console.log(err);
+        });
+      }
+    });
+  }
+
+  // fetching data in files via models
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+  // fetching data for Product Detail
+  static findById(id, cb) {
+    getProductsFromFile((products) => {
+      const product = products.find((p) => p.id === id);
+      cb(product);
+    });
+  }
+};
